4b357ec8b83d1d2d6c8b5548e0c4391fe4791129
0xjrr <pintoribeiro.josericardo@gmail.com>
2021-07-11 21:22:52 +0000

    Changes were made

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -11,24 +11,11 @@ app.get("/",(req,res)=>{
   });
 
 app.get("/url/emotion", (req,res) => {
-    console.log(req.text)
-    return res.send({"happy":"90","sad":"10"});
-});
-
-app.get("/url/sentiment", (req,res) => {
-    console.log(req)
-    return res.send("url sentiment for "+req.query.url);
-});
-
-app.get("/text/emotion", (req,res) => {
     console.log(Object.getOwnPropertyNames(req))
-
     console.log(req.query.text)
     const nluInstance = getNLUInstance()
-
     console.log(typeof nluInstance)
     console.log(Object.getOwnPropertyNames(nluInstance))
-
     const analyzeParams = {
         'features': {
             'emotion': {}
@@ -36,15 +23,97 @@ app.get("/text/emotion", (req,res) => {
         },
         'text': req.query.text
     };
-
     nluInstance.analyze(analyzeParams)
-        .then(analysisResults => {
+        .then((analysisResults,responseResults) => {
             console.log(JSON.stringify(analysisResults, null, 2));
+            responseResults= analysisResults.result.emotion.document.emotion;
+            console.log(responseResults)
+            console.log(typeof responseResults)
+            const textResponse = `Text sentiment for ${req.query.text} : 
+             sadness: ${responseResults.sadness}, 
+             joy: ${responseResults.joy}, 
+             fear: ${responseResults.fear}, 
+             disgust: ${responseResults.disgust}, 
+             anger: ${responseResults.anger}`
+            return res.send(responseResults);
         })
         .catch(err => {
             console.log('error:', err);
+            responseResults= JSON.parse(err.body).error;
+            return res.send(`Text emotion for \"${req.query.text}\": 
+            Error: ${responseResults}`);
         });
-    return res.send({"happy":"10","sad":"90"});
+});
+
+app.get("/url/sentiment", (req,res) => {
+    console.log(Object.getOwnPropertyNames(req))
+
+    console.log(req.query.text)
+    const nluInstance = getNLUInstance()
+
+    console.log(typeof nluInstance)
+    console.log(Object.getOwnPropertyNames(nluInstance))
+    let responseResults;
+    const analyzeParams = {
+        'features': {
+            'sentiment': {}
+
+        },
+        'text': req.query.text
+    };
+    
+    nluInstance.analyze(analyzeParams)
+        .then((analysisResults,responseResults) => {
+            console.log(JSON.stringify(analysisResults, null, 2));
+            responseResults= analysisResults.result.sentiment.document;
+            console.log(responseResults)
+            console.log(typeof responseResults)
+            return res.send(`text sentiment for \"${req.query.text}\": 
+            Score: ${responseResults.score}, 
+            Sentiment: ${responseResults.label}`);
+        })
+        .catch(err => {
+            console.log('error:', err);
+            responseResults= JSON.parse(err.body).error;
+            return res.send(`Text sentiment for \"${req.query.text}\": 
+            Error: ${responseResults}`);
+        });
+});
+
+app.get("/text/emotion", (req,res) => {
+    console.log(Object.getOwnPropertyNames(req))
+    console.log(req.query.text)
+    const nluInstance = getNLUInstance()
+    console.log(typeof nluInstance)
+    console.log(Object.getOwnPropertyNames(nluInstance))
+    const analyzeParams = {
+        'features': {
+            'emotion': {}
+
+        },
+        'text': req.query.text
+    };
+    nluInstance.analyze(analyzeParams)
+        .then((analysisResults,responseResults) => {
+            console.log(JSON.stringify(analysisResults, null, 2));
+            responseResults= analysisResults.result.emotion.document.emotion;
+            console.log(responseResults)
+            console.log(typeof responseResults)
+            const textResponse = `Text sentiment for ${req.query.text} : 
+             sadness: ${responseResults.sadness}, 
+             joy: ${responseResults.joy}, 
+             fear: ${responseResults.fear}, 
+             disgust: ${responseResults.disgust}, 
+             anger: ${responseResults.anger}`
+            return res.send(responseResults);
+        })
+        .catch(err => {
+            console.log('error:', err);
+            responseResults= JSON.parse(err.body).error;
+            return res.send(`Text emotion for \"${req.query.text}\": 
+            Error: ${responseResults}`);
+        });
+    
 });
 
 app.get("/text/sentiment", (req,res) => {
